ac64d637524a6b8dc99f0d453424f8442036309a
ascamach <ascamach@ucsc.edu>
2024-02-27 00:01:17 -0800

    Add button to move up/down stairs

---
+++ Moving On/Assets/Scripts/Player/MovePlayer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    [Header("Visual Cue")]
+    [SerializeField] private GameObject visualCue;
+
+    [Header("New Position")]
+    [SerializeField] private Vector3 newPosition;
+
+    private bool playerInRange;
+
+    private void Awake()
+    {
+        playerInRange = false;
+        visualCue.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (playerInRange)
+        {
+            visualCue.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                player.transform.position = newPosition;
+            }
+        }
+        else
+        {
+            visualCue.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+}
