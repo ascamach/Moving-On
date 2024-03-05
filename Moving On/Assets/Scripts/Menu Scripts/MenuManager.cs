f92221d7d54bd2a5f3cdf0595096d922a18f8415
tristynl <tristyn-lai@outlook.com>
2024-03-04 17:27:14 -0800

    Included How To Play Menu

--- Moving On/Assets/Scripts/Menu Scripts/MenuManager.cs
+++ Moving On/Assets/Scripts/Menu Scripts/MenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // https://www.youtube.com/watch?v=FfaG9TvCe5g - menu ui tutorial that was followed
 
@@ -9,5 +10,10 @@ public class MenuManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
