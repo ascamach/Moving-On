fa58a41b746ae5825f1d0361cec0b9c1ea63d309
Danny <dchan27@ucsc.edu>
2024-03-05 01:06:44 -0800

    made the disclaimer scene

---
+++ Moving On/Assets/Scripts/Menu Scripts/disclaimerDisplay.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// made the text red with this guide 
+// https://www.youtube.com/watch?v=kWAd7Qtn_Tw
+
+// disclaimer scene was set up following this 
+// https://chat.openai.com/share/aa87c400-776e-4c77-8b32-1146ca94433f
+
+public class disclaimerDisplay : MonoBehaviour
+{
+    public string MainMenu;
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            SceneManager.LoadSceneAsync("MainMenu");
+        }
+    }
+}
