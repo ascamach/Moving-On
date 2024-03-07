4d8386c7c949f556180efec17aac0b1c1816c3c5
ascamach <ascamach@ucsc.edu>
2024-03-06 22:43:02 -0800

    Add dialogue choices

---
+++ Moving On/Assets/Dialogue/test choices.ink
@@ -0,0 +1,16 @@
+-> main
+
+
+=== main ===
+Which number do you choose?
+    + [1]
+        -> chosen("1")
+    + [2]
+        -> chosen("2")
+    + [3]
+        -> chosen("3")
+        
+=== chosen(number) ===
+You chose {number}!
+
+-> DONE
--- Moving On/Assets/Scripts/Dialogue Scripts/DialogueManager.cs
+++ Moving On/Assets/Scripts/Dialogue Scripts/DialogueManager.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using Ink.Runtime;
+using UnityEngine.EventSystems;
 
 public class DialogueManager : MonoBehaviour
 {
+    // Variables needed for Dialogue UI
     [Header("Dialogue UI")]
     [SerializeField] private GameObject dialogueUI;
     [SerializeField] private TextMeshProUGUI dialogueText;
 
     private Story currentStory;
 
+    // Variables needed for Choices UI
+    [Header("Choices UI")]
+    [SerializeField] private GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
+
+    // Instance of dialogue playing
     private static DialogueManager instance;
     public bool dialoguePlaying { get; private set; }
 
     private void Awake()
     {
+        // Checks if there is more than one dialogue manager in the scene
         if (instance != null)
         {
             Debug.LogWarning("More than one instance of Dialogue Manager found in the scene.");
@@ -31,17 +40,30 @@ public class DialogueManager : MonoBehaviour
 
     private void Start()
     {
+        // Hides Dialogue UI at start of game
         dialoguePlaying = false;
         dialogueUI.SetActive(false);
+
+        // Gets all choices text
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach( GameObject choice in choices)
+        {
+            // Grabs the text of the choices buttons (children of the buttons)
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
     }
 
     private void Update()
     {
+        // Return if no dialogue is playing
         if(!dialoguePlaying)
         {
             return;
         }
 
+        // If dialogue is playing, the player can press space to progress through dialogue
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ContinueStory();
@@ -51,6 +73,8 @@ public class DialogueManager : MonoBehaviour
 
     public void EnterDialogueMode(TextAsset inkJSON)
     {
+        // Load the JSON file associated with the game object 
+        // Shows the Dialogue UI
         currentStory = new Story(inkJSON.text);
         dialoguePlaying = true;
         dialogueUI.SetActive(true);
@@ -60,6 +84,8 @@ public class DialogueManager : MonoBehaviour
 
     private void ExitDialogueMode()
     {
+        // After dialogue is finished, hide the Dialogue UI again
+        // and set the dialogue text to nothing.
         dialoguePlaying = false;
         dialogueUI.SetActive(false);
         dialogueText.text = "";
@@ -67,13 +93,56 @@ public class DialogueManager : MonoBehaviour
 
     private void ContinueStory()
     {
+        // If the dialogue is NOT on the last line, we can continue the story.
         if (currentStory.canContinue)
         {
             dialogueText.text = currentStory.Continue();
+            DisplayChoices();
         }
+        // If the next line in the Ink file is END, end the dialogue mode.
         else
         {
             ExitDialogueMode();
         }
     }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        // Checks if there are more choices than the number of UI buttons
+        if (currentChoices.Count > choices.Length)
+        {
+            Debug.Log("More choices than the UI supports. Number of choices given:" + currentChoices.Count);
+        }
+
+        int index = 0;
+        // Loads and initializes all available choices
+        foreach(Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+
+        for (int i = index; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        StartCoroutine(selectFirstChoice());
+    }
+
+    private IEnumerator selectFirstChoice()
+    {
+        // Defaults the first selected choice as the first choice in our list
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0]);
+    }
+
+    public void MakeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
+    } 
 }
