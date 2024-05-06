dfbd2212e37507c8e1439505f14283449f4092d8
ryuchannnnn <dchan27@ucsc.edu>
2024-05-06 00:31:14 -0700

    got the blur effect working

---
+++ Moving On/Assets/Scripts/Managers/InputScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class InputScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) 
+        {
+            PostProcessVolume ppVolume = Camera.main.gameObject.GetComponent<PostProcessVolume>();
+            ppVolume.enabled = !ppVolume.enabled;
+        }
+    }
+}
