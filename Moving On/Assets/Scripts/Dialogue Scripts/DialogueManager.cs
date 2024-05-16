9640b9b0c0f3d8c05f4dbc96826e781fd0e5ed49
ascamach <ascamach@ucsc.edu>
2024-05-15 19:38:00 -0700

    Reset alignment based on locale

--- Moving On/Assets/Scripts/Dialogue Scripts/DialogueManager.cs
+++ Moving On/Assets/Scripts/Dialogue Scripts/DialogueManager.cs
@@ -159,6 +159,7 @@ public class DialogueManager : MonoBehaviour
         }
         else
         {
+            dialogueText.alignment = TextAlignmentOptions.TopLeft;
             dialogueText.isRightToLeftText = false;
         }
 
