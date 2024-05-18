531e60a696f879ae9951ed2813c6d7c8590cc50a
wiwechen <129801256+wiwechen@users.noreply.github.com>
2024-05-18 02:29:26 -0700

    Adjusted Platforms, created Boxcast for Jumping, and readjust player movement

--- Moving On/Assets/Scripts/Player/playerMovement.cs
+++ Moving On/Assets/Scripts/Player/playerMovement.cs
@@ -9,18 +9,39 @@ public class playerMovement : MonoBehaviour
     [SerializeField] private float jumpPower;
     public bool isFacingRight = true;
 
-    [SerializeField] private GameObject ghost;
+
+    
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private bool dialogueInScene;
+
+    //Method 1 of Grounded
+    [SerializeField] private float rayCastDistance;
+
+    //Method 2 of Grounded
+    public Vector2 boxSize;
+    public float castDistance;
+
     
 
     private void Update()
     {
         Flip();
         //Debug.Log("Player Position: " + rb.position);
-        
+        if (dialogueInScene)
+        {
+            if (DialogueManager.GetInstance().dialoguePlaying)
+            {
+                // Deccelerate player
+                rb.velocity -= 0.1f * rb.velocity;
+                return;
+            }
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpPower * 50f));
+        }
     }
 
     private void FixedUpdate()
@@ -38,6 +59,8 @@ public class playerMovement : MonoBehaviour
 
         //horizontal = Input.GetAxis("Horizontal");
 
+        //Method 1 of Moving and Jumping
+        /*
         if (Input.GetKey(KeyCode.A)) {
             horizontal = -1f;
         } else if (Input.GetKey(KeyCode.D)) {
@@ -46,7 +69,7 @@ public class playerMovement : MonoBehaviour
         {
             horizontal = 0f;
         }
-
+        
         float yVelocity = rb.velocity.y;
 
         if (Input.GetButton("Jump") && isGrounded())
@@ -55,14 +78,52 @@ public class playerMovement : MonoBehaviour
         }
 
         rb.velocity = new Vector2(horizontal * speed, yVelocity);
+        */
+
+        //Method 2 of Moving and Jumping
+        if (Input.GetKey(KeyCode.A))
+        {
+            horizontal = -1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            horizontal = 1f;
+        }
+        else
+        {
+            horizontal = 0f;
+        }
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        
+
+
     }
+
+    //private bool isGrounded()
+    //{
+
+    //    RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayCastDistance, groundLayer); //Default at 0.85f
+    //    return (hit.collider != null);
+
+    //}
 
     private bool isGrounded()
     {
+        if(Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
         
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.9f, groundLayer);
-        return (hit.collider != null);
+    }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, boxSize);
     }
 
     private void Flip()
