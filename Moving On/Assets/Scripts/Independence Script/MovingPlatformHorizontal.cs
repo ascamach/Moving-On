848213639098dab162419204c3bd227205437a60
Danny <dchan27@ucsc.edu>
2024-06-03 19:28:09 -0700

    god please save mee

--- Moving On/Assets/Scripts/Independence Script/MovingPlatformHorizontal.cs
+++ Moving On/Assets/Scripts/Independence Script/MovingPlatformHorizontal.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MovingPlatformHorizontal : MonoBehaviour
 {
@@ -6,12 +8,23 @@ public class MovingPlatformHorizontal : MonoBehaviour
     public float minX = -5f;
     public float maxX = 5f;
 
+    public GameObject Player;
+
     void Update()
     {
         float newPositionX = Mathf.PingPong(Time.time * moveSpeed, maxX - minX) + minX;
         transform.position = new Vector3(newPositionX, transform.position.y, transform.position.z);
     }
+
+    private void OnCollisionEnter2D (Collision2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            Player.transform.parent = transform;
+        }
+    }  
+
+    private void OnCollisionExit2D (Collision2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            Player.transform.parent = null;
+        }
+    } 
 }
-
-
-
