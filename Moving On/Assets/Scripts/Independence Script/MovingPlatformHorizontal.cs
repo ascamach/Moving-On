d02eba4fde7550713791de6897b190ce4ead2098
ryuchannnnn <dchan27@ucsc.edu>
2024-06-13 20:33:43 -0700

    i did this on the wrong branch :l its not finished

--- Moving On/Assets/Scripts/Independence Script/MovingPlatformHorizontal.cs
+++ Moving On/Assets/Scripts/Independence Script/MovingPlatformHorizontal.cs
@@ -4,27 +4,61 @@ using System.Collections.Generic;
 
 public class MovingPlatformHorizontal : MonoBehaviour
 {
-    public float moveSpeed = 2f;
-    public float minX = -5f;
-    public float maxX = 5f;
+    // public float moveSpeed = 2f;
+    // public float minX = -5f;
+    // public float maxX = 5f;
 
-    public GameObject Player;
+    // public GameObject Player;
 
-    void Update()
-    {
-        float newPositionX = Mathf.PingPong(Time.time * moveSpeed, maxX - minX) + minX;
-        transform.position = new Vector3(newPositionX, transform.position.y, transform.position.z);
+    // void Update()
+    // {
+    //     float newPositionX = Mathf.PingPong(Time.time * moveSpeed, maxX - minX) + minX;
+    //     transform.position = new Vector3(newPositionX, transform.position.y, transform.position.z);
+    // }
+
+    // private void OnCollisionEnter2D (Collision2D other) {
+    //     if (other.gameObject.CompareTag("Player")) {
+    //         Player.transform.parent = transform;
+    //     }
+    // }  
+
+    // private void OnCollisionExit2D (Collision2D other) {
+    //     if (other.gameObject.CompareTag("Player")) {
+    //         Player.transform.parent = null;
+    //     }
+    // } 
+    public float speed;
+    public int startingPoint;
+    public Transform[] points;
+
+    private int i;
+
+    void Start() {
+        if (points.Length > 0) {
+            transform.position = points[startingPoint].position;
+        }
     }
 
-    private void OnCollisionEnter2D (Collision2D other) {
-        if (other.gameObject.CompareTag("Player")) {
-            Player.transform.parent = transform;
-        }
-    }  
+    void FixedUpdate() {
+        if (points.Length == 0) return;
 
-    private void OnCollisionExit2D (Collision2D other) {
-        if (other.gameObject.CompareTag("Player")) {
-            Player.transform.parent = null;
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f) {
+            i++;
+            if (i == points.Length) {
+                i = 0;
+            }
         }
-    } 
+
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.transform.CompareTag("Player")) {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+            collision.transform.SetParent(null);
+    }
 }
